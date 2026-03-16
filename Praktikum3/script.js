3133fe8e1d806070092af6ca0ae71c165f156a8b
halvinafarras <halvinaras@gmail.com>
2026-03-16 15:07:30 +0700

    ayas

---
+++ Praktikum3/script.js
@@ -0,0 +1,12 @@
+  let nama = "Ayas";
+        let umur = 30;
+        let isStudent = false;
+        let buah = ["Apel", "Mangga", "Jeruk"];
+        let mahasiswa = {nama: "Rina", usia: 20, jurusan: "Informatika"};
+
+        console.log(nama);
+        console.log(buah[1]);
+        console.log(mahasiswa.jurusan);
+
+        buah[1] = 'salak';
+        console.log(buah[1]);
